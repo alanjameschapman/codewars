0cf17bea8aac38888012d5280284257d1b7837e5
Alan Chapman <alanjameschapman@gmail.com>
2024-01-05 21:35:40 +0000

    Add js and py katas

--- js_kata.js
+++ js_kata.js
@@ -19,24 +19,57 @@
 
 // console.log(maps([1, 2, 3])) // returns [2, 4, 6]
 
-// 04/01/2024
-function countPositivesSumNegatives(input) {
-    if (input == null || input.length == 0) {
-        return [];
-    }
-    let countPositives = 0;
-    let sumNegatives = 0;
-    for (let i = 0; i < input.length; i++) {
-        let num = input[i]; // Added this line to assign the current number to the 'num' variable
-        if (num > 0) {
-            countPositives += 1;
-        } else { // Changed 'if' to 'else if' to avoid unnecessary checks
-            sumNegatives += num;
-        }
-    }
-    return [countPositives, sumNegatives];
-}
+// // 04/01/2024
+// function countPositivesSumNegatives(input) {
+//     if (input == null || input.length == 0) {
+//         return [];
+//     }
+//     let countPositives = 0;
+//     let sumNegatives = 0;
+//     for (let i = 0; i < input.length; i++) {
+//         let num = input[i]; // Added this line to assign the current number to the 'num' variable
+//         if (num > 0) {
+//             countPositives += 1;
+//         } else { // Changed 'if' to 'else if' to avoid unnecessary checks
+//             sumNegatives += num;
+//         }
+//     }
+//     return [countPositives, sumNegatives];
+// }
 
-output = countPositivesSumNegatives([-2, -1, 0, 1, 2]);
-console.log(output);
+// output = countPositivesSumNegatives([-2, -1, 0, 1, 2]);
+// console.log(output);
 
+// 05/01/2024
+// Rock Paper Scissors
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+// Examples(Input1, Input2 --> Output):
+// "scissors", "paper" --> "Player 1 won!"
+// "scissors", "rock" --> "Player 2 won!"
+// "paper", "paper" --> "Draw!"
+
+// const rps = (p1, p2) => {
+//     if (p1 == p2) return 'Draw!';
+//     if (p1 == 'rock' & p2 == 'scissors') return 'Player 1 won!';
+//     if (p1 == 'paper' & p2 == 'rock') return 'Player 1 won!';
+//     if (p1 == 'scissors' & p2 == 'paper') return 'Player 1 won!';
+//     else return 'Player 2 won';
+// };
+
+// console.log(rps('scissors', 'paper'));
+
+// 8 kyu - Keep up the hoop
+// Alex just got a new hula hoop, he loves it but feels discouraged because his
+//  little brother is better than him
+// Write a program where Alex can input (n) how many times the hoop goes round
+//  and it will return him an encouraging message :)
+// If Alex gets 10 or more hoops, return the string "Great, now move on to
+//  tricks".
+// If he doesn't get 10 hoops, return the string "Keep at it until you get it".
+
+function hoopCount (n) {
+    if (n < 10 ) return "Keep at it until you get it";
+    else return "Great, now move on to tricks";
+ }
+
+console.log(hoopCount(10));
--- py_kata.py
+++ py_kata.py
@@ -64,3 +64,33 @@
 # x = 2
 
 # print(check(a, x))
+
+# 05/01/2024
+# Implement a function which convert the given boolean value into its string
+#  representation.
+# Note: Only valid inputs will be given.
+
+# def boolean_to_string(b):
+#     return str(b)
+
+# b = True
+# print(boolean_to_string(b))
+
+# DESCRIPTION:
+# This function should test if the factor is a factor of base.
+# Return true if it is a factor or false if it is not.
+# About factors
+# Factors are numbers you can multiply together to get another number.
+# 2 and 3 are factors of 6 because: 2 * 3 = 6
+# You can find a factor by dividing numbers. If the remainder is 0 then the
+#  number is a factor.
+# You can use the mod operator (%) in most languages to check for a remainder
+# For example 2 is not a factor of 7 because: 7 % 2 = 1
+# Note: base is a non-negative number, factor is a positive number.
+
+# """function to test if the factor is a factor of base """
+# def check_for_factor(base, factor):
+#     return base%factor == 0
+
+# print(check_for_factor(6,2))
+# # print(7%2)
