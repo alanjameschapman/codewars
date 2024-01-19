f96dbc07f9952481ee9d76bf13299c98766551ca
Alan <alanjameschapman@gmail.com>
2024-01-19 10:20:36 +0000

    Add js and py kata

--- js_kata.js
+++ js_kata.js
@@ -399,3 +399,22 @@ const binaryArrayToNumber = arr => {
 }
 
 console.log(binaryArrayToNumber([0, 0, 0, 1]));
+
+// 19/01/2024
+// 7 kyu - Two to One
+// Take 2 strings s1 and s2 including only letters from ato z. Return a new
+//  sorted string, the longest possible, containing distinct letters,
+//  each taken only once - coming from s1 or s2.
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+function longest(s1, s2) {
+  let s3 = s1 + s2;
+  let s4 = s3.split('').sort();
+  let s5 = s4.filter((item, index) => s4.indexOf(item) === index);
+  return s5.join('');
+}
--- py_kata.py
+++ py_kata.py
@@ -396,3 +396,15 @@ def friend(x):
     return [name for name in x if len(name) == 4]
 
 print(friend(["Ryan", "Kieran", "Mark"]))
+
+# 19/01/2024
+# 8 kyu - What is between?
+# Complete the function that takes two integers (a, b, where a < b) and return
+#  an array of all integers between the input parameters, including them.
+# For example:
+# a = 1
+# b = 4
+# --> [1, 2, 3, 4]
+
+def between(a,b):
+    return list(range(a, b+1))
