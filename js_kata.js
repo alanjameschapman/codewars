4fde0dcc73d65f8098585fb0188dfec64350ad46
Alan <alanjameschapman@gmail.com>
2024-01-26 21:10:25 +0000

    Add js and py kata

--- js_kata.js
+++ js_kata.js
@@ -521,13 +521,36 @@ The test cases contain numbers only by mistake.
 // Write function isPalindrome that checks if a given string (case insensitive)
 //  is a palindrome.
 
-function isPalindrome(x) {
-  let reverse = x.split('').reverse().join('');
-  if (x.toLowerCase() === reverse.toLowerCase()) {
-    return true;
-  } else {
-    return false;
-  }
+// function isPalindrome(x) {
+//   let reverse = x.split('').reverse().join('');
+//   if (x.toLowerCase() === reverse.toLowerCase()) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(isPalindrome('anna'));
+
+// 26/01/2024
+// 7 kyu - Find the middle element
+// As a part of this Kata, you need to create a function that when provided with
+//  a triplet, returns the index of the numerical element that lies between the
+//  other two elements.
+// The input to the function will be an array of three distinct numbers
+//  (Haskell: a tuple).
+// For example:
+// gimme([2, 3, 1]) => 0
+// 2 is the number that fits between 1 and 3 and the index of 2 in the input
+//  array is 0.
+// Another example (just to make sure it is clear):
+// gimme([5, 10, 14]) => 1
+// 10 is the number that fits between 5 and 14 and the index of 10 in the input
+//  array is 1.
+
+function gimme(inputArray) {
+  let sorted = inputArray.slice().sort((a, b) => a - b);
+  return inputArray.indexOf(sorted[1]);
 }
 
-console.log(isPalindrome('anna'));
+console.log(gimme(2, 3, 1));
--- py_kata.py
+++ py_kata.py
@@ -488,7 +488,25 @@
 # my_list = ['Keep', 'Remove', 'Keep', 'Remove', 'Keep', ...]
 # None of the arrays will be empty, so you don't have to worry about that!
 
-def remove_every_other(my_list):
-    return my_list[::2]
+# def remove_every_other(my_list):
+#     return my_list[::2]
 
-print(remove_every_other(['Hello', 'Goodbye', 'Hello Again']))
+# print(remove_every_other(['Hello', 'Goodbye', 'Hello Again']))
+
+# 26/01/2024
+# 8 kyu - Keep Hydrated!
+# Nathan loves cycling.
+# Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres
+#  of water per hour of cycling.
+# You get given the time in hours and you need to return the number of litres
+#  Nathan will drink, rounded to the smallest value.
+# For example:
+# time = 3 ----> litres = 1
+# time = 6.7---> litres = 3
+# time = 11.8--> litres = 5
+
+def litres(time):
+    import math as m
+    return m.floor(0.5 * time)
+
+print(litres(3))
